ff77aabbab55fa066f117a593cc576366d5c3081
sdcohen <scohen@nvidia.com>
2017-07-30 13:34:45 -0400

    Add a script to help with how Darwin is constantly killing jobs

---
+++ test/sedovbig/legion_fill.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/Projects/pennant-legion/test/sedovbig
+for ver in -bdma ; do
+	systag=darwin-$SLURM_JOB_PARTITION
+	c=30
+	for x in 1 2 4 6 8 10 12 ; do 
+	for m in 16 12 10 8 6 4 2 1 ; do 
+		binding="-H $(scontrol show hostnames $SLURM_JOB_NODELIST | head -$(expr \( $m + 1 \) / 2) | tr '\n' ',') --map-by ppr:1:socket --bind-to socket"
+		for n in 16 12 10 8 6 4 2 1 ; do 
+				tag=${m}x$n${ver}-sedov${x}x${c}
+				if (( ! $(grep -c "hydro cycle" ${systag}-${tag}.log) > 0 )); then
+					mpirun -n $m $binding ../../pennant${ver} $(expr $m \* $n) ./sedovbig${x}x${c}.pnt \
+					-ll:cpu $n -ll:util 1 -ll:dma 1 -ll:csize 0 -ll:rsize 30000 -ll:stacksize 20 \
+					-lg:prof 4 -lg:prof_logfile trace-${tag}- 2>&1 > ${systag}-${tag}.log
+					~/Projects/legion/tools/legion_prof.py -f -o trace-${systag}-${tag} trace-${tag}-*.gz
+				fi
+			done
+		done
+	done
+done
