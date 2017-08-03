4d8d4960ba984703821f06c6cab7e9c656dca577
Saul Daniel Cohen - 315848 <scohen@nvidia.com>
2017-08-03 11:31:01 -0600

    Check in changes to scripts made while on Darwin
    	new file:   legion_debug.sh
    	modified:   legion_fill.sh
    	modified:   legion_test.sh
    	modified:   legion_weak.sh
    	modified:   mpi_fill.sh
    	modified:   mpi_test.sh
    	modified:   mpi_weak.sh
    	modified:   regent_fill.sh
    	modified:   regent_test.sh
    	modified:   regent_weak.sh

--- test/sedovbig/legion_debug.sh
+++ test/sedovbig/legion_debug.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 cd ~/Projects/pennant-legion/test/sedovbig
-for ver in -bdma{,-long,-wide}; do
+for ver in -debug ; do
 	systag=darwin-$SLURM_JOB_PARTITION
 	c=30
-	for x in 16 12 8 6 4 2 1 ; do
+	export GASNET_BACKTRACE=1
+	export DEBUG=1
+	for x in 12 ; do # 10 8 6 4 2 1 ; do
 		m=$x 
 		binding="-H $(scontrol show hostnames $SLURM_JOB_NODELIST | head -$(expr \( $m + 1 \) / 2) | tr '\n' ',') --map-by ppr:1:socket --bind-to socket"
 		n=$x; 
 		tag=${m}x$n${ver}-sedov${x}x${c}
-		echo ${systag}-${tag}
-		mpirun -n $m $binding ../../pennant${ver} $(expr $m \* $n) ./sedovbig${x}x${c}.pnt \
-		-ll:cpu $n -ll:util 1 -ll:dma 1 -ll:csize 30000 -ll:stacksize 20 \
-		-lg:prof $m -lg:prof_logfile trace-${tag}- 2>&1 > ${systag}-${tag}.log
+		mpirun -x DEBUG -x GASNET_BACKTRACE -n $m $binding /bin/time -v ../../pennant${ver} $(expr $m \* $n) ./sedovbig${x}x${c}.pnt \
+		-ll:cpu $n -ll:util 1 -ll:dma 1 -ll:rsize 0 -ll:csize 30000 -ll:stacksize 20 \
+		-lg:prof 4 -lg:prof_logfile trace-${tag}- 2>&1 > ${systag}-${tag}.log
 		~/Projects/legion/tools/legion_prof.py -f -o trace-${systag}-${tag} trace-${tag}-*.gz
 	done
 done
--- test/sedovbig/mpi_fill.sh
+++ test/sedovbig/mpi_fill.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-cd ~/Projects/pennant-legion/test/sedovbig
-for ver in -bdma ; do
-	systag=darwin-$SLURM_JOB_PARTITION
-	c=30
-	for x in 1 2 4 6 8 10 12 ; do 
-		for m in 16 12 10 8 6 4 2 1 ; do 
-			binding="-H $(scontrol show hostnames $SLURM_JOB_NODELIST | head -$(expr \( $m + 1 \) / 2) | tr '\n' ',') --map-by ppr:1:socket --bind-to socket"
-			for n in 16 12 10 8 6 4 2 1 ; do 
-				tag=${m}x$n${ver}-sedov${x}x${c}
-				echo ${systag}-${m}x${n}-mpi-sedov${x}x${c}
-				if [ -n "$(grep "hydro cycle" ${systag}-${m}x${n}-mpi-sedov${x}x${c}.log)" ]; then
-					OMP_NUM_THREADS=$n systag=$systag c=$c m=$m n=$n x=$x \
-					  mpirun -n $m $binding -x c -x m -x x -x n -x systag ./gprof_wrapper.sh
-					gprof ../../build/pennant gprof-${systag}-${m}x${n}-mpi-sedov${x}x${c}-*/gmon.out > gprof-${systag}-${m}x${n}-mpi-sedov${x}x${c}.gprof				fi
-			done
+cd ~/Projects/pennant-mpi/test/sedovbig
+systag=darwin-$SLURM_JOB_PARTITION
+c=30
+for x in 1 2 4 6 8 10 12 ; do 
+	for m in 16 12 10 8 6 4 2 1 ; do 
+		binding="-H $(scontrol show hostnames $SLURM_JOB_NODELIST | head -$(expr \( $m + 1 \) / 2) | tr '\n' ',') --map-by ppr:1:socket --bind-to socket"
+		for n in 16 12 10 8 6 4 2 1 ; do 
+			tag=${m}x$n-mpi-sedov${x}x${c}
+			echo ${systag}-${tag}
+			if [ ! -n "$(grep 'hydro cycle' ${systag}-${tag}-0.log)" ]; then
+				OMP_NUM_THREADS=$n systag=$systag c=$c m=$m n=$n x=$x \
+				  mpirun -n $m $binding -x c -x m -x x -x n -x systag ./gprof_wrapper.sh
+				gprof ../../build/pennant gprof-${systag}-${m}x${n}-mpi-sedov${x}x${c}-*/gmon.out > gprof-${systag}-${m}x${n}-mpi-sedov${x}x${c}.gprof
+			else
+				echo Already done
+			fi
 		done
 	done
 done
